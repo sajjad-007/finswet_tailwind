cc198c9ef8c4c992a8a50879c3a0166d1e9d53a2
sajjad-007 <sajjadhossain8123@gmail.com>
2023-12-28 15:39:13 +0600

    join section finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,8 @@ tailwind.config = {
         'hero-pattern': "url('/img/hero-pattern.svg')",
         'banner_img': "url('images/banner_img.jpg')",
         'overlay': "linear-gradient(360deg, #060606 16.68%, rgba(0, 0, 0, 0.02) 96.44%)",
+        'textgradiant': "linear-gradient(110deg, #A54E2B -53.83%, #DC9853 62.61%)",
+
 
       }
       }
