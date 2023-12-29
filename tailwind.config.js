d4ae9928ca74a89372d607ebb6107720a089c5a5
sajjad-007 <sajjadhossain8123@gmail.com>
2023-12-29 16:56:29 +0600

    serve section complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,9 +15,12 @@ tailwind.config = {
          backgroundImage: {
         'hero-pattern': "url('/img/hero-pattern.svg')",
         'banner_img': "url('images/banner_img.jpg')",
+        'serve': "url('images/serve.png')",
         'overlay': "linear-gradient(360deg, #060606 16.68%, rgba(0, 0, 0, 0.02) 96.44%)",
         'textgradiant': "linear-gradient(110deg, #A54E2B -53.83%, #DC9853 62.61%)",
+        
 
+        
 
       }
       }
